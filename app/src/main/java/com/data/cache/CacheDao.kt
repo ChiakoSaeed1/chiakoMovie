5076a870bacb21305a7f5bec0b85adb3f8b7dad9
saeed chiako <ssraby45@gmail.com>
2023-09-11 14:47:38 +0330

    cache dao

---
+++ app/src/main/java/com/data/cache/CacheDao.kt
@@ -0,0 +1,19 @@
+package com.data.cache
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import com.data.models.ResponseTopTv
+import com.utils.CACHE_TABLE_ENTITY
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CacheDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun saveCache(cacheEntity: CacheEntity)
+
+    @Query("SELECT * FROM $CACHE_TABLE_ENTITY")
+    fun getAllCache():Flow<List<ResponseTopTv>>
+}
